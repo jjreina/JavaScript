4b996cf51ba16f8bd260771cea409c49e9141486
jjreina <jreinapascual@gmail.com>
2023-06-12 20:54:40 +0200

    js basics - fuctions

--- 02-primitives/assets/js/fuctions.js
+++ 02-primitives/assets/js/fuctions.js
@@ -11,3 +11,17 @@ const arrowGreet = (name) => {
 
 greet("John", 43, "Spain");
 arrowGreet("Doe", 35, "Portugal");
+
+function addTwo(a, b) {
+  return a + b;
+}
+
+const addTwoArrow = (a, b) => {
+  return a + b;
+};
+
+const addTwoArrowLight = (a, b) => a + b;
+
+console.log(addTwo(2, 6));
+console.log(addTwoArrow(2, 6));
+console.log(addTwoArrowLight(2, 6));
