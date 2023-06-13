3c327e75fdca0c92a0fbb8de83128b370a0050ed
jjreina <jreinapascual@gmail.com>
2023-06-13 10:09:04 +0200

    js basics - cotrol structure

---
+++ 01-basics/assets/js/control-structure.js
@@ -0,0 +1,17 @@
+// if, else if, else
+// === operator
+
+const day = 3;
+
+// Alternative to if or switch
+const days = {
+  0: "Sunday",
+  1: "Monday",
+  2: "Tuesday",
+  3: "Wednesday",
+  4: "Thurday",
+  5: "Friday",
+  6: "Saturday",
+};
+
+console.log(days[day]);
