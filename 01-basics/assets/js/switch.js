22d1ea26c767876f02ce7469aa2cd37fd9760c74
jjreina <jreinapascual@gmail.com>
2023-06-13 18:51:42 +0200

    js basics - switch

---
+++ 01-basics/assets/js/switch.js
@@ -0,0 +1,16 @@
+const day = 0;
+
+switch (day) {
+  case 0:
+    console.log("Monday");
+    break;
+  case 1:
+    console.log("Tuesday");
+    break;
+  case 2:
+    console.log("Wednesday");
+    break;
+  default:
+    console.log("Other value");
+    break;
+}
