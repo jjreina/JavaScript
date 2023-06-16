da1cbe4a67826d1a744fafe984c71d9609b55836
jjreina <jreinapascual@gmail.com>
2023-06-16 15:06:26 +0200

    blackjack - implemented New Card

--- 02-blackjack/assets/js/game.js
+++ 02-blackjack/assets/js/game.js
@@ -5,9 +5,17 @@
  * 2S = Two of Spades
  */
 
-let deck = [];
 const TYPES_CARDS = ["C", "D", "H", "S"];
 const SPECIALS_CARDS = ["A", "J", "Q", "K"];
+
+let deck = [];
+let playerPoints = 0;
+let coputerPoints = 0;
+
+// References to HTNL
+const btnNewCard = document.querySelector("#btnNewCard");
+const playerScoreTag = document.querySelector("small");
+const playerCardDiv = document.querySelector("#player-cards");
 
 const createDeck = () => {
   // Normal cards, from 2 to 10
@@ -37,4 +45,22 @@ const takeCard = () => {
   return card;
 };
 
-takeCard();
+const cardValue = (card) => {
+  let value = card.substring(0, card.length - 1);
+  return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1; // to convert to number
+};
+
+let createCard = (card) => {
+  let imgCard = document.createElement("img");
+  imgCard.classList.add("my-card");
+  imgCard.src = `assets/cards/${card}.png`;
+  return imgCard;
+};
+
+// Events
+btnNewCard.addEventListener("click", () => {
+  let card = takeCard();
+  playerPoints = playerPoints + cardValue(card);
+  playerScoreTag.innerText = playerPoints;
+  playerCardDiv.append(createCard(card));
+});
